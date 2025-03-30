adf770bcb74597ff43fb20ab954b621a7a274cdb
Daniella Maleski <daniella.maleski@gmail.com>
2025-03-30 18:16:21 -0300

    aula sobre actions

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require("Cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    setupNodeEvents(on, config) {
+    },
+  },
+});
---
+++ cypress/e2e/appActionsTeste.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress" />
+
+const { email, senha } = require('../fixtures/data.json')
+const { profilePage } = require("../support/pages/profile.page")
+
+
+describe('Teste de Autenticação', () => {
+
+
+  it('deve fazer o login com sucesso', () => {
+    cy.login(email, senha)
+    profilePage.customerName().should('have.text', 'EBAC Cliente')
+  })
+})
